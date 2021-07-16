a34d0e6249b924fb94ef83371d68ced23ad92e48
Nataly <ny_amadorc@unicah.edu>
2021-07-15 23:07:17 -0600

    Hola

--- scripts/07_orden.sql
+++ scripts/07_orden.sql
@@ -1,10 +1,10 @@
 CREATE TABLE `carolina_jewerly_db`.`orden` (
-  `num_orden` INT NOT NULL AUTO_INCREMENT,
-  `codigo_orden` INT NOT NULL,
+  `cod_orden` VARCHAR(70) NOT NULL,
+  `num_orden` VARCHAR(60) NOT NULL,
   `codigo_cliente` INT NOT NULL,
   `fecha_emision` DATE NOT NULL,
-  `estado` VARCHAR(5) NOT NULL,
-  PRIMARY KEY (`num_orden`, `codigo_orden`),
+  `estado` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`num_orden`, `cod_orden`),
   INDEX `codigo_cliente_idx` (`codigo_cliente` ASC),
   CONSTRAINT `codigo_cliente`
     FOREIGN KEY (`codigo_cliente`)
